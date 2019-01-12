0199e63b26eb37b656f37e8fbe3edb6b4f708d90
Zhen Qiao <qiaozhen@huobi.com>
2019-01-11 21:28:37 -0800

    final

--- test.js
+++ test.js
@@ -1,51 +1,17 @@
-// lt --port 5000
-function formatPhoneNumber(phoneNumberString) {
-  var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
-  var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
-  if (match) {
-    var intlCode = (match[1] ? '+1 ' : '');
-    return [ '(', match[2], ')', match[3], '-', match[4]].join('')
-  }
-  return '-1'
+var b = '';
+
+function a(data) {
+   setTimeout(function () {
+     b = arguments[1];
+      console.log("var: ", data);
+   }, 500, '0 param','1 param');
 }
 
-function getName(userInput) {
-  let name = userInput.split(/[ ,]+/);
-  let firstNameIndex = name.indexOf('first');
-  let lastNameIndex = name.indexOf('last');
-  if(name.length <= 1) {
-    return name[0];
-  }else if(name.length <= 2) {
-    return name[0] + ' ' + name[1];
-  }else if(firstNameIndex !== -1) {
-    if(lastNameIndex !== -1) {
-      return name[firstNameIndex + 3] + ' ' + name[lastNameIndex + 3]
-    }else{
-      return name[firstNameIndex + 3]
-    }
-  }
-  if(name.length > 2 && name.length <= 4) {
-    return name[name.length - 1];
-  }
-  else if(name.length > 2 && name.length <= 5){
-    return name[name.length - 2] + ' ' + name[name.length - 1]
-  }
+a("param of func a");
+
+function c() {
+  setTimeout(function () {
+    console.log("b: ", b);
+  }, 600);
 }
-
-function change(user) {
-  user.syrups.push('abc');
-}
-
-/*
-const Action =  'Build';
-const Parameters:  { 'fields':
-   { 'Toppings': { 'listValue': [Object], 'kind': 'listValue' },
-     Type: { stringValue: '', kind: 'stringValue' },
-     Syrup: { listValue: [Object], kind: 'listValue' },
-     Flavor: { listValue: [Object], kind: 'listValue' },
-     Size: { stringValue: 'small', kind: 'stringValue' },
-     Container: { stringValue: 'cup', kind: 'stringValue' } } }
-
- */
-
-console.log(getName('George, my last name is Lee'));
+c();
